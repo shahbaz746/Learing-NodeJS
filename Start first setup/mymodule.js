c5d346c71d13f575b03f49a2d898439c49e283d4
Shahbaz <shahbazrao445@gmail.com>
2025-09-27 15:49:05 +0500

    Remove node_modules from tracking

---
+++ Start first setup/mymodule.js
@@ -0,0 +1,13 @@
+export const a = 1 //named export
+export const b = 4 //named export
+export const c = 3 //named export
+export const d = 4 //named export
+
+//defult export
+let obj;
+ obj = {
+    x:5,
+    y:7
+}
+
+export default obj;
---
+++ Start first setup/server.js
@@ -0,0 +1,10 @@
+// import {a} from "./mymodule.js"
+// console.log(a)
+
+
+
+import obj from "./mymodule.js"
+console.log(obj )
+
+// require way    
+// const a = require("./mymodule2.js")
